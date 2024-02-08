ed047d6a0368181295713cde78fdeb12a2520c62
lezernack <luks.ezck@gmail.com>
2024-02-08 16:02:07 -0700

    got everything working, but pictures won't load

---
+++ script.js
@@ -0,0 +1,47 @@
+const URL =
+  "https://iskarr.github.io/austindonovan.github.io/api/business.json";
+
+let cards = document.querySelector("div.cards");
+let para = document.createElement("p");
+
+fetch(URL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    let business = jsObject.business;
+
+    for (let i = 0; i < business.length; i++) {
+      // console.log(business[i].name);
+      // You must include your javascript below to display the following information:
+      // 1. business names, 2.business image,
+      // 3. business locations, 4. business descriptions
+      // Here is an example for the name to start you off.
+
+      // Creates card and places business name in h2 element
+      let card = document.createElement("section");
+      // Adds a classname to the section element above
+      card.className = "card";
+      let h2 = document.createElement("h2");
+      h2.textContent = business[i].name;
+      card.appendChild(h2);
+      document.querySelector("div.cards").appendChild(card);
+
+      // Create the image location, you can look up how to add image using javascript
+      let image = document.createElement("img");
+      image.src = business[i];
+      image.alt = business[i].name;
+      card.appendChild(image);
+      document.querySelector("div.cards").appendChild(image);
+
+      // Include a business location
+      let address = document.createElement("h3");
+      address.textContent = business[i].address;
+      card.appendChild(address);
+      document.querySelector("div.cards").appendChild(address);
+
+      // Include a business Description
+      let description = document.createElement("p");
+      description.textContent = business[i].description;
+      card.appendChild(description);
+      document.querySelector("div.cards").appendChild(description);
+    }
+  });
